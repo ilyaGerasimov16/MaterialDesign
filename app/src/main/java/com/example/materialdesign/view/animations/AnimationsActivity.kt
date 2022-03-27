a12083b05397e48ad539151a2b87979696ec173c
Ilya Gerasimov <ilya.gerasimov.97@mail.ru>
2022-03-27 15:39:51 +0300

    Урок 5. Transition Explode(), (добавили кнопки через RecyclerView)

--- app/src/main/java/com/example/materialdesign/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/example/materialdesign/view/animations/AnimationsActivity.kt
@@ -1,12 +1,16 @@
 package com.example.materialdesign.view.animations
 
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import androidx.transition.*
+import android.view.LayoutInflater
 import android.view.View
-import androidx.transition.Slide
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.*
+import com.example.materialdesign.R
 import com.example.materialdesign.databinding.ActivityAnimationsBinding
+
 
 class AnimationsActivity : AppCompatActivity() {
 
@@ -18,21 +22,46 @@ class AnimationsActivity : AppCompatActivity() {
         binding = ActivityAnimationsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener{
-            val transitionSet = TransitionSet()
-            val slide = Slide(Gravity.START)
-            //val explode = Explode()
-            slide.duration = 2000
-            //explode.duration = 2000
-            transitionSet.ordering = TransitionSet.ORDERING_SEQUENTIAL
+        binding.recyclerView.adapter = MyAdapter()
 
-            transitionSet.addTransition(slide)
-            //transitionSet.addTransition(explode)
+    }
 
-            TransitionManager.beginDelayedTransition(binding.transitionContainer, transitionSet)
+    inner class MyAdapter : RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
+        inner class MyViewHolder(view:View):RecyclerView.ViewHolder(view){
 
-            textIsVisible = !textIsVisible
-            binding.text.visibility = if(textIsVisible) View.VISIBLE else View.GONE
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.activity_animations_recycler_item, parent,false))
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.itemView.setOnClickListener{
+                val transitionSet = TransitionSet()
+                val explode = Explode()
+                val fade = Fade()
+                fade.duration = 99999999999
+                explode.duration = 3000
+                val rect1 = Rect()
+                it.getGlobalVisibleRect(rect1)
+                explode.excludeTarget(it,true)
+
+                explode.epicenterCallback = object : Transition.EpicenterCallback(){
+                    override fun onGetEpicenter(transition: Transition): Rect {
+                        return rect1
+                    }
+
+                }
+                transitionSet.addTransition(explode)
+                transitionSet.addTransition(fade)
+                TransitionManager.beginDelayedTransition(binding.transitionContainer, transitionSet)
+                binding.recyclerView.adapter = null
+
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return 32
         }
     }
 }
