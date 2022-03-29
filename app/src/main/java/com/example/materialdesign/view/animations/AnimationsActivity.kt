60ca6195e852b76503f715ac7fb23cf57104198a
Ilya Gerasimov <ilya.gerasimov.97@mail.ru>
2022-03-29 23:41:00 +0300

    Урок 5. познакомились с ConstraintSet()

--- app/src/main/java/com/example/materialdesign/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/example/materialdesign/view/animations/AnimationsActivity.kt
@@ -10,32 +10,52 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnticipateInterpolator
+import android.view.animation.AnticipateOvershootInterpolator
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 import com.example.materialdesign.R
 import com.example.materialdesign.databinding.ActivityAnimationsBinding
+import com.example.materialdesign.databinding.ActivityAnimationsBonusStartBinding
 
 
 class AnimationsActivity : AppCompatActivity() {
 
 
-    private var flag = false
+    private var flag = true
     private val duration = 1000L
-    lateinit var binding: ActivityAnimationsBinding
+
+    lateinit var binding: ActivityAnimationsBonusStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsBonusStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.backgroundImage.setOnClickListener{
+            flag = !flag
+            if (flag){
+                val constraintSet = ConstraintSet()
+                val changeBounds = ChangeBounds()
+                changeBounds.duration = 1000L
+                changeBounds.interpolator = AnticipateOvershootInterpolator(1f)
+                constraintSet.clone(this,R.layout.activity_animations_bonus_start)
+                TransitionManager.beginDelayedTransition(binding.constraintContainer, changeBounds)
+                constraintSet.applyTo(binding.constraintContainer)
+            } else{
+                val constraintSet = ConstraintSet()
+                val changeBounds = ChangeBounds()
+                changeBounds.duration = 1000L
+                changeBounds.interpolator = AnticipateOvershootInterpolator(1f)
+                constraintSet.clone(this,R.layout.activity_animations_bonus_end)
+                TransitionManager.beginDelayedTransition(binding.constraintContainer, changeBounds)
+                constraintSet.applyTo(binding.constraintContainer)
+            }
 
-        binding.scrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            binding.header.isSelected = binding.scrollView.canScrollVertically(-1)
         }
 
     }
-
-
 }
